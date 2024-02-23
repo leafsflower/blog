f1db1373fde0373d1a73de82b2404d9c98e90b46
leafs <afengstamp@qq.com>
2024-02-23 22:44:03 +0800

    v2

--- demoBlog/settings.py
+++ demoBlog/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-4ytufgz81ynp*)an=hm7&%7d0vza%j9yl!gma!!tb5g@l^le%i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 
